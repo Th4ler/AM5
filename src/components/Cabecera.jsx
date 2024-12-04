8e2bd74430840fdfdcd358007e68e4ab27f9c06e
Th4ler <thaler.toro@gmail.com>
2024-12-04 01:34:47 -0300

    Styling the 'Cabecera' section with improved responsiveness.

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,29 +1,40 @@
 //Home: Imagen cabecera
 import Image from 'react-bootstrap/Image';
-import am5cabecera from '../images/imagenhome.webp';
 import am5logo from '../assets/am5-logo.png';
 
 function Cabecera() {
     return (
-        <section id="inicio" className='d-flex flex-column' data-component="cabecera">
-            <div className="cabecera" style={{ height: '100vh', backgroundImage: `url(${am5cabecera})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                
-                <div className="text-center col-md-4 offset-md-7-5">
-                    <div className='box-cabecera'>
-                        <div className='row'>
-                            <Image className='col-md-3' src={am5logo} alt='Logo de AM5, startup de soluciones legales y comunicación estratégica.' fluid style={{ width: '140px', height: 'auto', objectFit: 'contain' }} />
-                            
+        <>
+            <section
+                id="inicio"
+                className="cabecera"
+                data-component="cabecera"
+            >
+                <div className="content-wrapper text-center align-items-end" style={{ flex: 1 }}>
+                    {/* Contenido en la parte derecha */}
+                    <div className="box-cabecera col-md-5 me-4">
+                        <div className="row align-items-center justify-content-end">
+                            <Image
+                                className='col-auto d-none d-md-flex ps-0'
+                                src={am5logo}
+                                alt="Logo de AM5, startup de soluciones legales y comunicación estratégica."
+                                fluid
+                            />
                             <div className="right-corner"></div>
-                            <h1 className='text-muted col-md-8'>Soluciones legales<br></br> Negocios y<br></br> Comunicación estratégica</h1>
+                            <h1 className="text-muted col-auto">
+                                Soluciones legales<br />
+                                Negocios y<br />
+                                Comunicación estratégica
+                            </h1>
                         </div>
-                    </div>                  
+                    </div>
+
                 </div>
+                {/* <div className='cabecera-hr'></div> */}
                 
-            </div>
-            {/* <div className='cabecera-hr'></div> */}
-            
+            </section>
             <div className='custom-hr rounded w-75 mb-4'></div>
-        </section>
+        </>
     );
 }
 
