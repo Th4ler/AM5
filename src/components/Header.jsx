6c21a75f89770afaa5d141c59a0bd16afe34da26
Th4ler <thaler.toro@gmail.com>
2024-12-04 22:38:58 -0300

    Correción de barra inferior amarilla en móviles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ import logo from '../assets/am5-logo.png';
 function Header() {
     return (
         <Navbar sticky="top" bg="white" expand="lg" className="Navbar">
-            <Navbar.Brand href="#home" className="logo">
+            <Navbar.Brand href="/" className="logo">
                 <img src={logo} alt="Imagen del logo de la start-up AM5 con su característico toque de color amarillo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav variant="underline" defaultActiveKey="#inicio" className="ms-auto custom-underline-color">
+                <Nav defaultActiveKey="#inicio" className="ms-auto custom-underline-color" style={{ borderBottom: 'none' }}>
                     <Nav.Link href="#inicio">Inicio</Nav.Link>
                     <Nav.Link href="#about">Sobre Mí</Nav.Link>
                     <Nav.Link href="#services">Servicios</Nav.Link>
