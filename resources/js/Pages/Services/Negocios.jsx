f41ea514c8f2163eb260f70d1b54513b4cb733ff
Th4ler <thaler.toro@gmail.com>
2025-01-11 03:53:48 -0300

    Negocios Component v1.0

--- resources/js/Pages/Services/Negocios.jsx
+++ resources/js/Pages/Services/Negocios.jsx
@@ -2,17 +2,26 @@
 //Libreria flipcard -> https://www.npmjs.com/package/reactjs-flip-card -> npm install reactjs-flip-card
 //Demo llibreria -> https://react-flip-card-demo.surge.sh/
 import { useState } from 'react';
+import LazyLoad from 'react-lazy-load';
 import ReactFlipCard from 'reactjs-flip-card';
 import Header from '../Landing/Header';
 import logo from '../../../../public/am5-logo.png';
 import aboutmePic from '../../../../public/images/aboutmePic.webp';
-import BackgroundImage from '../../../../public/images/soluciones-legales.webp';
-import CorporateImage from '../../../../public/images/corporate-building.jpg';
+import BackgroundImage from '../../../../public/images/Negocios.webp';
+import Image1 from '../../../../public/images/adultWorking.webp';
+import Image2 from '../../../../public/images/businessWomen.webp';
+import Image3 from '../../../../public/images/graphicDesigner.webp';
+import Image4 from '../../../../public/images/increaseInflation.jpg';
+import Image5 from '../../../../public/images/manDeveloper.webp';
+import Image6 from '../../../../public/images/companyMeeting.webp';
+import Image7 from '../../../../public/images/marketing.webp';
+import Spinner from '../../Components/Spinner';
 
-const LegalCard = ({ title, description, image }) => {
+const BusinessCards = ({ title, description, image }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     const cardStyles = {
         front: {
-            background: `url(${image}) no-repeat center center / cover`,
             borderRadius: '12px',
             width: '400px',
             height: '225px',
@@ -20,6 +29,7 @@ const LegalCard = ({ title, description, image }) => {
             flexDirection: 'column',
             justifyContent: 'flex-end',
             border: '3px solid #ccc',
+            overflow: 'hidden',
         },
         back: {
             background: '#fff',
@@ -35,6 +45,13 @@ const LegalCard = ({ title, description, image }) => {
         }
     };
 
+    // Precargar la imagen
+    const handleImagePreload = () => {
+        const img = new Image();
+        img.src = image;
+        img.onload = () => setImageLoaded(true);
+    };
+
     return (
         <ReactFlipCard
             flipTrigger="onHover"
@@ -42,9 +59,33 @@ const LegalCard = ({ title, description, image }) => {
             frontStyle={cardStyles.front}
             backStyle={cardStyles.back}
             frontComponent={
-                <div className="bg-gray-200 text-center flex flex-row items-center justify-center gap-2 p-2">
-                    <img src={logo} alt="Logo" className="w-10 h-10 logo" />
-                    <p className="text-gray-700 font-semibold m-0">{title}</p>
+                <div className="h-full w-full">
+                    <LazyLoad 
+                        height={225}
+                        offset={300}
+                        threshold={0.95}
+                        onContentVisible={handleImagePreload}
+                    >
+                        <div className="relative w-full h-full">
+                            {!imageLoaded && <Spinner />}
+                            
+                            <div 
+                                className={`absolute inset-0 w-full h-full transition-opacity duration-500 ${
+                                    imageLoaded ? 'opacity-100' : 'opacity-0'
+                                }`}
+                                style={{
+                                    backgroundImage: `url(${image})`,
+                                    backgroundPosition: 'center',
+                                    backgroundSize: 'cover',
+                                    backgroundRepeat: 'no-repeat'
+                                }}
+                            />
+                        </div>
+                    </LazyLoad>
+                    <div className="bg-gray-200 text-center flex flex-row items-center justify-center gap-2 p-2 absolute bottom-0 w-full z-10">
+                        <img src={logo} alt="Logo" className="w-10 h-10 logo" />
+                        <p className="text-gray-700 font-semibold m-0">{title}</p>
+                    </div>
                 </div>
             }
             backComponent={
@@ -87,18 +128,13 @@ const ServiceIntro = ({ showDescription, toggleDescription }) => (
 );
 
 const LEGAL_SOLUTIONS = [
-    { id: 1, title: 'Corporativo & MA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: CorporateImage },
-    { id: 2, title: 'Venture Capital', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 3, title: 'Laboral y Migración', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 4, title: 'Inmobiliario', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 5, title: 'Impuestos', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 6, title: 'Litigios y Arbitrajes', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 7, title: 'Insolvencia y Reorganización', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 8, title: 'Compliance', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 9, title: 'Consumo y Publicidad', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 10, title: 'Propiedad industrial e Intelectual', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 11, title: 'Bancario Financiero y Fintech', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
-    { id: 12, title: 'Protección de Datos', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: aboutmePic },
+    { id: 1, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image1 },
+    { id: 2, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image2 },
+    { id: 3, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image3 },
+    { id: 4, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image4 },
+    { id: 5, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image5 },
+    { id: 6, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image6 },
+    { id: 7, title: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: Image7 },
 ];
 
 const SolucionesLegales = () => {
@@ -116,7 +152,7 @@ const SolucionesLegales = () => {
 
             <div className="flex justify-center items-center gap-6 py-10 text-center flex-wrap">
                 {LEGAL_SOLUTIONS.map(solution => (
-                    <LegalCard
+                    <BusinessCards
                         key={solution.id}
                         title={solution.title}
                         description={solution.description}
